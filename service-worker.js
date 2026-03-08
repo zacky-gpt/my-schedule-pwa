8627c92cfc3d7c10362d4e10606c100d9f25a7d6
zacky-gpt <inaction522@gmail.com>
2026-03-08 23:46:13 +0900

    Upgrade service worker to version 4

--- service-worker.js
+++ service-worker.js
@@ -1,32 +1,31 @@
-const CACHE = 'backward-scheduler-v3';
-const ASSETS = ['./', './index.html', './manifest.json', './service-worker.js'];
-
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', event => {
-  const { request } = event;
-  if (request.mode === 'navigate') {
-    event.respondWith(
-      fetch(request)
-        .then(response => {
-          const copy = response.clone();
-          caches.open(CACHE).then(cache => cache.put('./index.html', copy));
-          return response;
-        })
-        .catch(() => caches.match('./index.html'))
-    );
-    return;
-  }
-
-  event.respondWith(caches.match(request).then(cached => cached || fetch(request)));
-});
+const CACHE = 'backward-scheduler-v4';
+const ASSETS = ['./', './index.html', './manifest.json', './service-worker.js'];␊
+␊
+self.addEventListener('install', event => {␊
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));␊
+  self.skipWaiting();␊
+});␊
+␊
+self.addEventListener('activate', event => {␊
+  event.waitUntil(␊
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))␊
+  );␊
+  self.clients.claim();␊
+});␊
+␊
+self.addEventListener('fetch', event => {␊
+  const { request } = event;␊
+  if (request.mode === 'navigate') {␊
+    event.respondWith(␊
+      fetch(request)␊
+        .then(response => {␊
+          const copy = response.clone();␊
+          caches.open(CACHE).then(cache => cache.put('./index.html', copy));␊
+          return response;␊
+        })␊
+        .catch(() => caches.match('./index.html'))␊
+    );␊
+    return;␊
+  }␊
+␊
+  event.respondWith(caches.match(request).then(cached => cached || fetch(request)));␊
